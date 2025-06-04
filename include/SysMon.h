d0fa44868593514acf5666a4b71239c9f0a7413c
Yacine20elfadili <168276351+Yacine20elfadili@users.noreply.github.com>
2025-06-04 12:54:41 +0000

    Update SysMon.h

--- include/SysMon.h
+++ include/SysMon.h
@@ -1,110 +1,29 @@
-#ifndef SYSMON_H
-#define SYSMON_H
-
-#include "CpuMonitor.h"
-#include "MemoryMonitor.h"
-#include "ProcessMonitor.h"
+#ifndef CPUMONITOR_H
+#define CPUMONITOR_H
 
 #include <string>
-#include <chrono>
-#include <thread>
-#include <sstream>
-#include <iomanip>
-#include <ctime>
 
-class SysMonitor : public CpuMonitor, public MemoryMonitor, public ProcessMonitor {
-private:
-    int updateInterval;  // Update interval in milliseconds
-    bool fullLog;        // Flag for full logging
+using namespace std;
 
-    std::string getTime() {
-        auto now = std::chrono::system_clock::now();
-        auto time = std::chrono::system_clock::to_time_t(now);
-        std::stringstream ss;
-        ss << std::put_time(std::localtime(&time), "%Y-%m-%d %H:%M:%S");
-        return ss.str();
-    }
+class CpuMonitor {
+protected:
+    struct CPU{
+        short nbrCPU;
+        float *usagePerCPU;
+        float usageCPU;
+        float frequency;
+        float frequencyMax;
+    }CPU;
 
 public:
-    SysMonitor(int interval = 1000, bool enableFullLog = false) 
-        : CpuMonitor(), 
-          MemoryMonitor(), 
-          ProcessMonitor(),
-          updateInterval(interval),
-          fullLog(enableFullLog) {}
+    CpuMonitor();
+    
+    bool update();
+    float getCpuUsage();
+    float getCpuFreq();
+    string getCpuInfo();
 
-    void setUpdateInterval(int interval) {
-        updateInterval = interval;
-    }
-    int getUpdateInterval() {
-        return updateInterval ;
-    }
-
-    void setFullLogging(bool enable) {
-        fullLog = enable;
-    }
-    bool getFullLogging() {
-        return fullLog;
-    }
-
-    bool update() {
-        return CpuMonitor::update() && 
-               MemoryMonitor::update() && 
-               ProcessMonitor::update();
-    }
-
-    std::string getInfo(std::string type) {
-        if (type == "cpu") {
-            return getCpuInfo();
-        }
-        else if (type == "memory") {
-            return "Memory Usage: " + std::to_string(getMemoryUsagePercentage()) + "% " +
-                   "Swap Usage: " + std::to_string(getSwapUsagePercentage()) + "%\n";
-        }
-        else if (type == "process") {
-            return getProcessInfo();
-        }
-        return "Unknown info type";
-    }
-
-    std::string exportAsText() {
-        std::stringstream ss;
-        ss << "=== System Monitor Report ===\n";
-        ss << "Time: " << getTime() << "\n\n";
-        ss << "--- CPU Information ---\n" << getInfo("cpu") << "\n";
-        ss << "--- Memory Information ---\n" << getInfo("memory") << "\n";
-        ss << "--- Process Information ---\n" << getInfo("process") << "\n";
-        return ss.str();
-    }
-
-    std::string exportAsCSV() {
-        std::stringstream ss;
-        ss << "Timestamp,CPU Usage,CPU Frequency,Memory Usage,Swap Usage\n";
-        ss << getTime() << "," 
-           << getCpuUsage() << "," 
-           << getCpuFreq() << "," 
-           << getMemoryUsagePercentage() << "," 
-           << getSwapUsagePercentage() << "\n";
-        return ss.str();
-    }
-
-
-    int run(){
-    try {
-        while (true) {
-            if (update()) {
-            this_thread::sleep_for(std::chrono::milliseconds(updateInterval));
-            }
-        }
-    }
-    catch (const exception& e) {
-        cerr << "error: " << e.what() << endl;
-        return 1;
-    }
-    return 0;
-}
-
-    ~SysMonitor(){};
+    ~CpuMonitor();
 };
 
 #endif
