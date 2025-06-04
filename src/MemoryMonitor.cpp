cb0e5e58afaf3b9350ae12bf77074ccbf8376a0e
Yacine20elfadili <168276351+Yacine20elfadili@users.noreply.github.com>
2025-06-04 12:56:58 +0000

    Update MemoryMonitor.cpp

--- src/MemoryMonitor.cpp
+++ src/MemoryMonitor.cpp
@@ -6,11 +6,19 @@ MemoryMonitor::MemoryMonitor() {
     RAM.freeMem = 0;
     RAM.SwapMeminMb = 0;
     RAM.freeSwp = 0;
+    RAM.usage = 0;
+    RAM.usageSwp = 0;
 }
 
 
 bool MemoryMonitor::update() {
-    return false; 
+    RAM.totalMemInMb = 16384;      // 16 GB total RAM
+    RAM.freeMem = 8192;            // 8 GB free RAM
+    RAM.SwapMeminMb = 8192;        // 8 GB total swap
+    RAM.freeSwp = 6144;            // 6 GB free swap
+    RAM.usage = RAM.totalMemInMb - RAM.freeMem;  // Used RAM
+    RAM.usageSwp = RAM.SwapMeminMb - RAM.freeSwp;  // Used swap
+    return true; 
 }
 
 unsigned long long MemoryMonitor::getTotalMemory() {
@@ -21,20 +29,49 @@ unsigned long long MemoryMonitor::getFreeMemory() {
     return (RAM.freeMem * 1024 * 1024);
 }
 
+unsigned long long MemoryMonitor::getUsedMemory() {
+    return (RAM.usage * 1024 * 1024);
+}
+
 unsigned long long MemoryMonitor::getTotalSwap() {
     return (RAM.SwapMeminMb * 1024 * 1024);
 }
 
+unsigned long long MemoryMonitor::getFreeSwap() {
+    return (RAM.freeSwp * 1024 * 1024);
+}
+
 unsigned long long MemoryMonitor::getUsedSwap() {
-    return 0; 
+    return (RAM.usageSwp * 1024 * 1024);
 }
 
 double MemoryMonitor::getMemoryUsagePercentage() {
-    return 0; 
+    if (RAM.totalMemInMb == 0) return 0.0;
+    return ((double)RAM.usage / RAM.totalMemInMb) * 100.0;
 }
 
 double MemoryMonitor::getSwapUsagePercentage() {
-    return 0; 
+    if (RAM.SwapMeminMb == 0) return 0.0;
+    return ((double)RAM.usageSwp / RAM.SwapMeminMb) * 100.0;
+}
+
+std::string MemoryMonitor::getMemoryInfo() {
+    const double MB_TO_GB = 1024.0;
+    
+    std::string info = "System Memory Information\n";
+    info += "----------------------------------------\n";
+    info += "RAM Total: " + std::to_string(RAM.totalMemInMb / MB_TO_GB) + " GB\n";
+    info += "RAM Free:  " + std::to_string(RAM.freeMem / MB_TO_GB) + " GB\n";
+    info += "RAM Used:  " + std::to_string(RAM.usage / MB_TO_GB) + " GB\n";
+    info += "RAM Usage: " + std::to_string(getMemoryUsagePercentage()) + "%\n";
+    info += "----------------------------------------\n";
+    info += "Swap Total: " + std::to_string(RAM.SwapMeminMb / MB_TO_GB) + " GB\n";
+    info += "Swap Free:  " + std::to_string(RAM.freeSwp / MB_TO_GB) + " GB\n";
+    info += "Swap Used:  " + std::to_string(RAM.usageSwp / MB_TO_GB) + " GB\n";
+    info += "Swap Usage: " + std::to_string(getSwapUsagePercentage()) + "%\n";
+    info += "========================================\n";
+    
+    return info;
 }
 
 MemoryMonitor::~MemoryMonitor() {}
